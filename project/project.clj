02e9f83780d729da6a072effd4060bae7a26814c
Jason Thai <jasonnthaiii@gmail.com>
2018-04-17 22:48:27 -0700

    managed to get some cascalog working in terminal with these dependencies (Eclipse IDE was bugging)

--- project/project.clj
+++ project/project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cascalog/cascalog-core "3.0.0"]
+                 [org.apache.hadoop/hadoop-core "1.2.1"]
+                 ])
+  :jvm-opts ["-Xms768m" "-Xmx768m"]
+  
