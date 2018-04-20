f5c41125de5efa29b7bd47418bd3318b1c98056c
Jason Thai <jasonnthaiii@gmail.com>
2018-04-20 15:38:37 -0700

    main

--- project/project.clj
+++ project/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cascalog/cascalog-core "3.0.0"]
                  [org.apache.hadoop/hadoop-core "1.2.1"]
-                 ])
+                 ]
   :jvm-opts ["-Xms768m" "-Xmx768m"]
+  :main project.core)
   
--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -1,7 +1,12 @@
 (ns project.core)
 (use 'cascalog.api)
-(use 'cascalog.playground)
+(use 'cascalog.playground) (bootstrap)
 
 (defn test1
   ([]     (test1 "Hello world!"))
   ([msg]  (println msg)))
+
+(defn -main
+  "This should be pretty simple."
+  []
+  (println "Hello, World!"))
