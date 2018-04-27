1405c281158e1639e886125945d1546661f15707
Jason Thai <jasonnthaii@gmail.com>
2018-04-27 14:03:39 -0700

    split the csv into elements

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -1,6 +1,7 @@
 (ns project.core)
 (require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io])
+         '[clojure.java.io :as io]
+         '[clojure.string :as str])
 (use 'cascalog.api)
 (use 'cascalog.playground) (bootstrap)
 
@@ -8,10 +9,13 @@
   ([]     (test1 "Hello world!"))
   ([msg]  (println msg)))
 
+
 (defn -main
   []
   (test1)
   (println (io/resource "airline_delay_causes.csv"))
   (?- (stdout)sentence)
-  (println (slurp (io/resource "airline_delay_causes.csv")))
+  (def x (str/split(slurp (io/resource "airline_delay_causes.csv")) #","))
+  ;x now contains each element of the CSV seperated into a list
+  (println x)
   )
