ac1a08a489046fce9410c85038ad9a32024883c7
Jason Thai <jasonnthaii@gmail.com>
2018-05-04 14:45:55 -0700

    proper dependencies, working on hdfs parsing

--- project/project.clj
+++ project/project.clj
@@ -7,6 +7,11 @@
                  [cascalog/cascalog-core "3.0.0"]
                  [org.apache.hadoop/hadoop-core "1.2.1"]
                  [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/data.json "0.1.1"]
+                 [cascalog "1.8.3"]
+                 [cascalog-contrib "1.0.0-SNAPSHOT"]
                  ]
   :jvm-opts ["-Xms768m" "-Xmx768m"]
   :main project.core)
--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -5,66 +5,51 @@
 (use 'cascalog.api)
 (use 'cascalog.playground) (bootstrap)
 
+(defn- parse-str [^String s]
+  (seq (.split s ",")))
+
+(defn longify [str]
+  (Long/parseLong str))
+
+(defmapcatop split [sentence]
+    (seq (.split sentence "\\s+")))
+
+
+(def reaction-data
+  (let [source (hfs-textline "/home/jason/Downloads/CS152/gitproject/CS-152-Project/project/resources/airline_delay_causes.csv")]
+    (<- [?line]
+        (source ?line))
+    ))
+
+;(def reaction-data
+;  (let [source (hfs-textline "/home/jason/Downloads/CS152/gitproject/CS-152-Project/project/resources/airline_delay_causes.csv")]
+;   (<- [?reaction ?to]
+;       (source ?line)
+;       (parse-str ?line :> _ _ _ ?reaction ?to _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _)
+;       (:distinct false))
+;   ))
+
+(comment
+  (defnk first-n
+    [gen n :sort nil :reverse false]
+    (let [in-fields (get-out-fields gen)
+          out-vars  (v/gen-nullable-vars (count in-fields))]
+      (<- out-vars
+          (gen :>> in-fields)
+          (:sort :<< (if sort (collectify sort)))
+          (:reverse reverse)
+          (c/limit [n] :<< in-fields :>> out-vars))))
+  )
+
 (defn test1
   ([]     (test1 "Hello world!"))
   ([msg]  (println msg)))
 
-; Returns a list 4th, 7th, 8th element 
-(defn names [x] (list (nth x 3) (nth x 6) (nth x 7)))
-
-;; param: list of lists and calls names on each list
-(defn genNames [x] (if (< (count x) 21) []
-                     (do 
-                       (def entry (names x))
-                       (def remaining (drop 10 x))
-                       entry
-                     )  
-                     ))
-
-;; Calculate percentage delay
-;; param: num - number of flights 
-;; param: total - number of delayed flights
-;; divide total by num
-(defn percentage-delay [num total]
-  (* (/ total num) 100))
-
-;; Clojure only provides first and second
-;; Return nth element ?
-(def fourth #(nth % 3)) ; Carrier
-(def seventh #(nth % 6)) ; Total number of flights
-(def eighth #(nth % 7)) ; total number of delays
-
-;; Extract the interesting part
-;; Return list of forth, seventh and eigth
-(def extract (juxt fourth seventh eighth))
-
-;; Parse the data
-(defn parse [airname-data-row]
-;; let -> pass in a list of 3 values
-  (let [[date open close] (extract airname-data-row)]
-    [date (percentage-delay (Double/parseDouble open)
-                             (Double/parseDouble close))]))
-
-(defn max-change [csv-file]
-  (with-open [reader (io/reader csv-file)]
-    (->> (rest (csv/read-csv reader))
-         (map parse)
-         (apply max-key second))))
-
 (defn -main
   []
-  (test1)
-  (println (io/resource "airline_delay_causes.csv"))
+  ;(println (parse-str "\"year\",\" month\",\"carrier\",\"carrier_name\",\"airport\",\"airport_name\",\"arr_flights\",\"arr_del15\",\"carrier_ct\",\" weather_ct\",\"nas_ct\",\"security_ct\",\"late_aircraft_ct\",\"arr_cancelled\",\"arr_diverted\",\" arr_delay\",\" carrier_delay\",\"weather_delay\",\"nas_delay\",\"security_delay\",\"late_aircraft_delay\","))
+  (println (reaction-data 100))
+  ;(test1)
+  ;(println (io/resource "airline_delay_causes.csv"))
   ;(?- (stdout)sentence)
-  (def x (str/split(slurp (io/resource "airline_delay_causes.csv")) #","))
-  ;x now contains each element of the CSV seperated into a list
-  (def y (parse ["2018" "1" "\"9E\"" "\"Endeavor Air Inc.\"" "\"ABY\"" "\"Albany  GA: Southwest Georgia Regional\"" "83.00" "10.00" "3.60" "0.98" "1.51" "0.00" "3.91" "3.00" "0.00" "685.00" "106.00" "240.00" "43.00" "0.00" "296.00"]))
-  (println)
-  (println y)
-  ; (def z (rest(with-open [reader (io/reader (io/resource "airline_delay_causes.csv"))]
-  ; (doall
-  ;   (csv/read-csv reader)))))
-  ;(println z)
-  (println)
-  ;(def x1 (max-change (io/resource "airline_delay_causes.csv")))
   )
