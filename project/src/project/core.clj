31106780850795b27baed8996efe2b9d784cc466
Jason Thai <jasonnthaii@gmail.com>
2018-05-04 17:54:16 -0700

    can get absolute path of resources not much progress

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -8,48 +8,32 @@
 (defn- parse-str [^String s]
   (seq (.split s ",")))
 
-(defn longify [str]
-  (Long/parseLong str))
+(defn- parse-strings [^String name]
+  (hfs-textline name))
 
-(defmapcatop split [sentence]
-    (seq (.split sentence "\\s+")))
+(def say-hello
+ (fn [name]
+   (str "Hello " name)))
 
+;;not working. Intention is to parse line by line carrier and flight data
+(def airline-data
+  (let [source (hfs-textline (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))]
+   (<- [?airline ?flights]
+       (source ?line)
+       ;;we are only looking for the 4th column(carrier name) and 7th column (total flights)
+       (parse-str ?line :> _ _ _ ?airline _ _ ?flights _ _ _ _ _ _ _ _ _ _ _ _ _ _)
+       (:distinct false))
+   ))
 
-(def reaction-data
-  (let [source (hfs-textline "/home/jason/Downloads/CS152/gitproject/CS-152-Project/project/resources/airline_delay_causes.csv")]
-    (<- [?line]
-        (source ?line))
-    ))
-
-;(def reaction-data
-;  (let [source (hfs-textline "/home/jason/Downloads/CS152/gitproject/CS-152-Project/project/resources/airline_delay_causes.csv")]
-;   (<- [?reaction ?to]
-;       (source ?line)
-;       (parse-str ?line :> _ _ _ ?reaction ?to _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _)
-;       (:distinct false))
-;   ))
-
-(comment
-  (defnk first-n
-    [gen n :sort nil :reverse false]
-    (let [in-fields (get-out-fields gen)
-          out-vars  (v/gen-nullable-vars (count in-fields))]
-      (<- out-vars
-          (gen :>> in-fields)
-          (:sort :<< (if sort (collectify sort)))
-          (:reverse reverse)
-          (c/limit [n] :<< in-fields :>> out-vars))))
-  )
-
-(defn test1
-  ([]     (test1 "Hello world!"))
-  ([msg]  (println msg)))
 
 (defn -main
   []
-  ;(println (parse-str "\"year\",\" month\",\"carrier\",\"carrier_name\",\"airport\",\"airport_name\",\"arr_flights\",\"arr_del15\",\"carrier_ct\",\" weather_ct\",\"nas_ct\",\"security_ct\",\"late_aircraft_ct\",\"arr_cancelled\",\"arr_diverted\",\" arr_delay\",\" carrier_delay\",\"weather_delay\",\"nas_delay\",\"security_delay\",\"late_aircraft_delay\","))
-  (println (reaction-data 100))
-  ;(test1)
-  ;(println (io/resource "airline_delay_causes.csv"))
-  ;(?- (stdout)sentence)
+  (println (say-hello "Jason"))
+  (println (list (airline-data 10)))
+  ;test gets path of our csv file resource
+  (println (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))
+  
+  
+  ;test parse-str on single example line of our csv
+  (println (parse-str "\"year\",\" month\",\"carrier\",\"carrier_name\",\"airport\",\"airport_name\",\"arr_flights\",\"arr_del15\",\"carrier_ct\",\" weather_ct\",\"nas_ct\",\"security_ct\",\"late_aircraft_ct\",\"arr_cancelled\",\"arr_diverted\",\" arr_delay\",\" carrier_delay\",\"weather_delay\",\"nas_delay\",\"security_delay\",\"late_aircraft_delay\","))
   )
