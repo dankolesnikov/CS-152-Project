1fb8b46658874715633d768bfcde7b0ae91fcc35
Jason Thai <jasonnthaii@gmail.com>
2018-05-05 14:50:25 -0700

    list of years

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -32,9 +32,29 @@
 (defn delay-by-carrier 
   "Query outputs all carrier names with corresponding arrival delay time repl usage: (delay-by-carrier)"
   []
-  (?<- (stdout) [?carrier_name ?arr_delay]
+  (?<- (stdout) [?carrier_name ?year ?arr_flights ?arr_delay]
     (flight-data ?line)
     ;(flight-parser ?line :> headers)))
+    (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
+                            ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+                            ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+                            ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+                            ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+                            ?empty)))
+
+(def get-names
+  (<- [?carrier_name]
+    (flight-data ?line)
+    (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
+                            ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+                            ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+                            ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+                            ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+                            ?empty)))
+
+(def get-years
+  (<- [?year]
+    (flight-data ?line)
     (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
                             ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
                             ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
@@ -63,28 +83,20 @@
  (fn [name]
    (str "Hello " name)))
 
-;;not working. Intention is to parse line by line carrier and flight data
-; (def airline-data
-;   (let [source (hfs-textline (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))]
-;    (<- [?airline ?flights]
-;        (source ?line)
-;        ;;we are only looking for the 4th column(carrier name) and 7th column (total flights)
-;        (parse-str ?line :> _ _ _ ?airline _ _ ?flights _ _ _ _ _ _ _ _ _ _ _ _ _ _)
-;        (:distinct false))
-;    ))
-;; Path to simple text file "/Users/danil/Desktop/textfile.txt"
-; (def print-test 
-;   (let [hfs-delimite (text-line "/Users/danil/Desktop/textfile.txt")]
-;   (?<- (stdout) [?textline]
-;       (text-tap ?textline)))
-; )   
+(defn print-test 
+  []
+  (?<- (stdout) [?name]
+    (get-names ?name)))
 
+(def years 
+  (list get-years))
 
 (defn -main
   []
   (println "Starting...")
-  (delay-by-carrier) ; outputs carriers carriers and delay times
+  (println years)
+  ;(delay-by-carrier) ; outputs carriers carriers and delay times
   (println "Writing CSV...")
-  (write-to-csv) ; creates csv file from parsed data
+  ;(write-to-csv) ; creates csv file from parsed data
   (println "DONE")
   )
