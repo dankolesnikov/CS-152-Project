d6a1573894d9668d2368360db5d70c5794fd954c
Avi Banerjee <abanerjee@scholastic.com>
2018-05-05 15:23:29 -0700

    working on averaging predicates

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -1,12 +1,86 @@
 (ns project.core)
-(require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io]
-         '[clojure.string :as str])
+(require '[clojure.java.io :as io]
+         '[clojure.string :as str]
+         '[clojure.data.csv :as csv])
+
 (use 'cascalog.api)
 (use 'cascalog.playground) (bootstrap)
 
 (defn- parse-str [^String s]
   (seq (.split s ",")))
+
+; csv data file
+(def flight-data (hfs-textline "resources/airline_delay_causes_2018.csv"))
+
+(defn flight-parser 
+  "parses csv file"
+  [line]
+  (map #(.trim %) (first (csv/read-csv line))))
+
+; not working
+(defn headers
+  "Return headers of csv file; make syntax easier for queries"
+  []
+  ; ([?year ?month ?carrier ?carrier_name ?airport
+  ;                           ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+  ;                           ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+  ;                           ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+  ;                           ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+  ;                           ?empty])
+)
+  
+(defn delay-by-carrier 
+  "Query outputs all carrier names with corresponding arrival delay time repl usage: (delay-by-carrier)"
+  []
+  (?<- (stdout) [?carrier_name ?year ?arr_flights ?arr_delay]
+    (flight-data ?line)
+    ;(flight-parser ?line :> headers)))
+    (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
+                            ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+                            ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+                            ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+                            ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+                            ?empty)))
+
+(def get-names
+  (??<- [?carrier_name]
+    (flight-data ?line)
+    (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
+                            ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+                            ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+                            ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+                            ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+                            ?empty)))
+
+(def get-years
+  (??<- [?year]
+    (flight-data ?line)
+    (flight-parser ?line :> ?year ?month ?carrier ?carrier_name ?airport
+                            ?airport_name ?arr_flights ?arr_del15 ?carrier_ct 
+                            ?weather_ct ?nas_ct ?security_ct ?late_aircraft_ct 
+                            ?arr_cancelled ?arr_diverted ?arr_delay ?carrier_delay 
+                            ?weather_delay ?nas_delay ?security_delay ?late_aircraft_delay 
+                            ?empty)))
+
+(def years 
+  (distinct get-years))
+
+(def names 
+  (distinct get-names))
+
+; Partially working
+; Need delay-by-carrier to actually return. What is stdout? Need to change that
+; Right now it just executes the job
+(defn write-to-csv
+  "Create a CSV file with the parsed data from delay-by-carrier query"
+  []
+  (with-open [out-file (clojure.java.io/writer "carrier_delay.csv")]
+              (clojure.data.csv/write-csv out-file [
+              ["carrier_name" "arrival_delay"] 
+              ["test" "test"]
+              [(delay-by-carrier)]
+              ]))
+)                            
 
 (defn- parse-strings [^String name]
   (hfs-textline name))
@@ -15,44 +89,19 @@
  (fn [name]
    (str "Hello " name)))
 
-;;not working. Intention is to parse line by line carrier and flight data
-(def airline-data
-  (let [source (hfs-textline (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))]
-   (<- [?airline ?flights]
-       (source ?line)
-       ;;we are only looking for the 4th column(carrier name) and 7th column (total flights)
-       (parse-str ?line :> _ _ _ ?airline _ _ ?flights _ _ _ _ _ _ _ _ _ _ _ _ _ _)
-       (:distinct false))
-   ))
-
-(def basic-ass-data
-  (let [source (hfs-textline (.getPath(io/resource "Test.txt")))]
-   (<- [?line]
-       (source ?line))
-   ))
-
-(def print-test 
-  (let [text-tap (hfs-textline (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))]
-  (?<- (stdout) [?textline]
-      (text-tap ?textline)))
-)   
+(defn print-test 
+  []
+  (?<- (stdout) [?name]
+    (get-names ?name)))
 
 
 (defn -main
   []
-  (def items (basic-ass-data 2))
-  (println items)
-  ;(let [text-tap (hfs-textline (.getPath(io/resource "Test.txt")))]
-  ;   (?<- (println) [?textline]
-  ;       (text-tap ?textline)))
-  (println (basic-ass-data 2))
-  (println (say-hello "Jason"))
-  (println (airline-data 10))
-  ;test gets path of our csv file resource
-  ;(println print-test)
-  (println (.getPath(io/resource "airline_delay_causes_2012_2017.csv")))
-  
-  
-  ;test parse-str on single example line of our csv
-  (println (parse-str "\"year\",\" month\",\"carrier\",\"carrier_name\",\"airport\",\"airport_name\",\"arr_flights\",\"arr_del15\",\"carrier_ct\",\" weather_ct\",\"nas_ct\",\"security_ct\",\"late_aircraft_ct\",\"arr_cancelled\",\"arr_diverted\",\" arr_delay\",\" carrier_delay\",\"weather_delay\",\"nas_delay\",\"security_delay\",\"late_aircraft_delay\","))
+  (println "Avi branch...")
+  (println "Starting...")
+  (println (distinct names))
+  ;(delay-by-carrier) ; outputs carriers carriers and delay times
+  (println "Writing CSV...")
+  ;(write-to-csv) ; creates csv file from parsed data
+  (println "DONE")
   )
