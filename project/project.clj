f05f80063150da811c6cf352e3c6a94d6b3198c1
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2018-05-04 18:41:05 -0700

    project.clj modified to use insurece connection, doesnt work otherwise

--- project/project.clj
+++ project/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cascalog/cascalog-core "3.0.0"]
                  [org.apache.hadoop/hadoop-core "1.2.1"]
@@ -15,4 +15,9 @@
                  ]
   :jvm-opts ["-Xms768m" "-Xmx768m"]
   :main project.core)
+
+;; allow insecure downloads
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+ "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
   
--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -37,3 +37,4 @@
   ;test parse-str on single example line of our csv
   (println (parse-str "\"year\",\" month\",\"carrier\",\"carrier_name\",\"airport\",\"airport_name\",\"arr_flights\",\"arr_del15\",\"carrier_ct\",\" weather_ct\",\"nas_ct\",\"security_ct\",\"late_aircraft_ct\",\"arr_cancelled\",\"arr_diverted\",\" arr_delay\",\" carrier_delay\",\"weather_delay\",\"nas_delay\",\"security_delay\",\"late_aircraft_delay\","))
   )
+  
