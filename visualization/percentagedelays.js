59886202557e4fbf9aa87d9775c13b24bc884bd0
Avi Banerjee <abanerjee@scholastic.com>
2018-05-08 01:00:19 -0700

    parse csv for airline and percentage delay. Note: must add headers 'airline, percentage_delay'

--- visualization/percentagedelays.js
+++ visualization/percentagedelays.js
@@ -1,5 +1,11 @@
+d3.csv(
+	"average_delays_2015.csv", function(data) {
 
+		.row(function(d){
+			return {
+				airline:String(d.airline),percentage_delay:Number();
+			}
+		})
 
-d3.csv("average_delays_2015.csv", function(data) {
-  console.log(data);
-});
+  		
+		});
