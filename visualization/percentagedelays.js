06de5600acb9677603bc5b4d87b30be07895e762
Avi Banerjee <abanerjee@scholastic.com>
2018-05-08 01:26:23 -0700

    casting percentage delay to double

--- visualization/percentagedelays.js
+++ visualization/percentagedelays.js
@@ -1,11 +1,19 @@
-d3.csv(
-	"average_delays_2015.csv", function(data) {
+d3.csv("average_delays_2015.csv", function(data) {
 
-		.row(function(d){
-			return {
-				airline:String(d.airline),percentage_delay:Number();
-			}
-		})
+			console.log(data);
+				
+				
+			.row(function(d){ return airline, percentage_delay:Number(d.percentage_delay.slice(1,2))})
 
-  		
+			var height = 100;
+  			var width = 100;
+
+  			var delay = parseAll(percentage_delay);
+  			console.log(delay);
+
+  			var minDelay = d3.min(data, function(d){ return d.percentage_delay; });
+  			var max = d3.max(data, function(d) { return d.column2; });
+
+  			//console.log(minDelay);
+  			//console.log(max);
 		});
