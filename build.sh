21ea9bc5c8e565ac03d1d01e097314622119bbcf
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2018-05-09 02:35:48 -0700

    buld update

--- build.sh
+++ build.sh
@@ -14,18 +14,18 @@ git clone $repo
 echo "Done cloning."	
 cd "CS-152-Project"	
 # less "project-proposal.txt"	
-echo "Installing Leiningen - build tool for Clojrue projects."	
+# echo "Installing Leiningen - build tool for Clojrue projects."	
 	
 # This doesn't get the latest version" sudo apt-get install clojure	
 # Below is the preffered way to install lein	
-cp leiningen-build-script.sh /usr/local/bin	
-sudo chmod +x /usr/local/bin/lein/leiningen-build-script	
-lein -version	
+# cp leiningen-build-script.sh /usr/local/bin	
+# sudo chmod +x /usr/local/bin/lein/leiningen-build-script	
+# lein -version	
 	
-echo "Creating a Hadoop JAR"	
-lein uberjar	
-echo "Uploading JAR to Google Cloud"	
-echo "Downloading CSV files from Google Cloud"
-echo "CSV files generated from Google Cloud Hadoop cluster reside in hadoop_output folder"
+# echo "Creating a Hadoop JAR"	
+# lein uberjar	
+# echo "Uploading JAR to Google Cloud"	
+# echo "Downloading CSV files from Google Cloud"
+# echo "CSV files generated from Google Cloud Hadoop cluster reside in hadoop_output folder"
 echo "Opening slides.."
 xdg-open "slides/reveal.js-3.6.0/finalPresentation.html"
