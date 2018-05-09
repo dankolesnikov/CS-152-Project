31d428a5bf79e99102bbac13766ab84f9ad75112
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2018-05-09 02:31:07 -0700

    slides update

---
+++ visualization/percentagedelays.js
@@ -0,0 +1,12 @@
+var data = d3.csv("average_delays_2015.csv", function(data) {
+
+	console.log(data);
+ 	d.forEach(function(data) {
+      d.percentage_delay = +d.Percentage_delay;
+    
+    }); 
+   // console.log(data);
+});
+
+console.log(data);
+
