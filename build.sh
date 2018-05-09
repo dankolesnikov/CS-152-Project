7e9b9623a6b6d79a2374e378ed07eb432d7b6409
Jason Thai <jasonnthaii@gmail.com>
2018-05-09 03:18:36 -0700

    better build script that has some functionaliy. Can FULLY work on any single node cluster

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@
 echo "CS 152 Cascalog Project"
 echo "Starting to build..." 
 echo "Cloning repo"
+
+cd /tmp
  	 
-repo="https://github.com/dankolesnikov/CS-152-Project.git"	+repo = "https://github.com/dankolesnikov/CS-152-Project.git"
-git clone $repo
+git clone "https://github.com/dankolesnikov/CS-152-Project.git"
 echo "Done cloning."	
 cd "CS-152-Project"	
 # less "project-proposal.txt"	
@@ -18,14 +19,19 @@ cd "CS-152-Project"
 	
 # This doesn't get the latest version" sudo apt-get install clojure	
 # Below is the preffered way to install lein	
-# cp leiningen-build-script.sh /usr/local/bin	
-# sudo chmod +x /usr/local/bin/lein/leiningen-build-script	
-# lein -version	
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod +x lein	
+sudo mv lein /usr/local/bin
 	
-# echo "Creating a Hadoop JAR"	
-# lein uberjar	
+lein -version	
+	
+echo "Creating a Hadoop JAR"	
+lein uberjar	
 # echo "Uploading JAR to Google Cloud"	
 # echo "Downloading CSV files from Google Cloud"
 # echo "CSV files generated from Google Cloud Hadoop cluster reside in hadoop_output folder"
-echo "Opening slides.."
-xdg-open "slides/reveal.js-3.6.0/finalPresentation.html"
+
+java -jar project-0.1.0-SNAPSHOT-standalone.jar
+
+echo "Opening results of project"
+xdg-open "csv_output"
